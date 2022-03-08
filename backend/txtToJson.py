83b7a5714d650820fc2f59db0c5a82607ce3ca2f
Nahom  Atsbeha <nahomatsbeha@Nahoms-MacBook-Pro.local>
2022-03-08 13:12:06 -0500

    added a python file that turns a single text file into a json file with the format {chap#: {verse#: verse}}

---
+++ backend/txtToJson.py
@@ -0,0 +1,25 @@
+import json
+
+
+filename = "./txtfileofbooks/exodus1.txt"
+
+book = {}
+chap = {}
+
+with open(filename) as fh:
+    bookname = fh.readline()
+
+    for line in fh:
+        verse = line.split("፤", 1)
+        # print(verse)
+        if verse[0] != "\n":
+            chap[verse[0]] = verse[1]
+    
+    book[bookname] = chap
+
+    #json_string = json.dumps(dict1, ensure_ascii = False).encode('utf8')
+    # print(json_string.decode())
+
+with open("exodus1.json", "w", encoding='utf8') as json_file:
+    json.dump(book, json_file, ensure_ascii=False)
+# json_file.close()
